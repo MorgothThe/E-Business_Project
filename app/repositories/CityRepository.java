5f3689f9fbc46c147bfaa960abbdcf4f7ae3cbe4
Mokry667 <mokry667@gmail.com>
2017-11-14 15:44:40 +0100

    add genericRepository

---
+++ app/repositories/CityRepository.java
@@ -0,0 +1,12 @@
+package repositories;
+
+import models.City;
+
+public class CityRepository extends GenericRepository<City> {
+
+    public CityRepository(){
+        super(City.class);
+    }
+
+}
+
