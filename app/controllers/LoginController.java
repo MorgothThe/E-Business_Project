7462c618669dfa3f195cc50bec3c8c9502540639
Piotr Herba <220658@student.pwr.edu.pl>
2017-11-26 09:58:24 +0100

    flash messages addeded

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -34,22 +34,25 @@ public class LoginController extends Controller {
             if(passwordManager.isPasswordCorrect(password, salt, hash))
             {
                 session("username", username);
-
+                flash("logged-in", "User has been succesfully logged in!");
                 return redirect("/");
             }
             else
             {
-                return ok("PASSWORD INCORRECT");
+                flash("password-incorect", "The password is not correct");
+                return redirect("/");
             }
         }
         else
         {
-            return ok("USER DOES NOT EXIST");
+            flash("no-user", "The user does not exist");
+            return redirect("/");
         }
     }
 
     public Result logout(){
 
+        flash("logged-out", "You have successfully logged out");
         session().remove("username");
 
         return redirect("/");
