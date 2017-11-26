c27e7dd895fdf8f2867e6e15d7595da12a139a81
Piotr Herba <220658@student.pwr.edu.pl>
2017-11-26 10:21:15 +0100

    js fix, on document ready

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,8 @@
-$('.message a').click(function () {
-    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
-    $('h2').animate({opacity: "toggle"});
-})
+$('document').ready(function(){
+    $('.message a').click(function () {
+        console.log($(this));
+        $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+        $('h2').animate({opacity: "toggle"});
+    });
+});
+
