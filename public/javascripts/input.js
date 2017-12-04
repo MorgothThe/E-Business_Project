bb2c98ee9ca08768993d6b1851c4b9fadcf2685d
Tenzin Choewang <tchoedol@gmail.com>
2017-12-04 23:00:21 +0100

    Profile and final fix

---
+++ public/javascripts/input.js
@@ -0,0 +1,6 @@
+﻿$('document').ready(function () {
+    $('.edit-btn').click(function () {
+        $('.profile-input').animate({ opacity: "toggle" }, "slow");
+        $('.letters p').animate({ opacity: "toggle" },500);
+    });
+});
